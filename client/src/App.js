3003175dc65595245a5c78943af1e366e6276a75
Karla <mazariek@gmail.com>
2022-03-19 11:54:40 -0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
 import Home from "./Pages/Home/Home"
-// import Cart from './Pages/Cart/Cart';
-// import Login from './Pages/Login/Login';
-// import Product from './Pages/Product/Product';
-// import ProductList from './Pages/ProductList/ProductList';
-// import Register from './Pages/Register/Register';
+import Cart from './Pages/Cart/Cart';
+import Login from './Pages/Login/Login';
+import Product from './Pages/Product/Product';
+import ProductList from './Pages/ProductList/ProductList';
+import Register from './Pages/Register/Register';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 
 
 
 function App() {
+  const user = true;
   return (
     <div className="App">
-        <Home></Home>
-        {/* <Cart></Cart> */}
-        {/* <Login></Login> */}
-        {/* <Product></Product> */}
-        {/* <ProductList></ProductList> */}
-        {/* <Register></Register> */}
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/products/:category">
+            <ProductList />
+          </Route>
+          <Route path="/product/:id">
+            <Product />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+          <Route path="/register">
+            {user ? <Redirect to="/" /> : <Register />}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
