b3e1fd8d2e6e02758f77c50315a70cfed3a128b0
Karla <mazariek@gmail.com>
2022-04-11 17:07:27 -0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./Pages/Home/Home"
 import Cart from './Pages/Cart/Cart';
 import Login from './Pages/Login/Login';
 import Product from './Pages/Product/Product';
+import Success from './Pages/Success'
 import ProductList from './Pages/ProductList/ProductList';
 import Register from './Pages/Register/Register';
 import {
@@ -22,6 +23,7 @@ function App() {
         <Route path="/products/:category" element={<ProductList />}></Route>
         <Route path="/product/:id" element={<Product />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/success" element={<Success />}></Route>
         <Route path="/login" element={user ? <Login/> : <Home />}></Route>
         <Route path="/register" element={user ? <Register/> : <Home />}></Route>
       </Routes>
