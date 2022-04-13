b8ab0f3eb89e24361719581df1b36665a0e60b91
Karla <mazariek@gmail.com>
2022-04-12 21:52:05 -0700

    commit

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -47,6 +47,10 @@ const Button = styled.button`
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+  &:disabled {
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
